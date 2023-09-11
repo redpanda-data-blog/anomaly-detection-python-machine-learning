3ba5a8ab85beb1ac5d41d32c2ab850a3c51e6847
Artem Oppermann <artem.oppermann@uni-oldenburg.de>
2023-09-11 13:12:19 +0200

    Update anomaly_detection_with_redpanda.py

--- anomaly_detection_with_redpanda.py
+++ anomaly_detection_with_redpanda.py
@@ -1,85 +1,101 @@
-#Import all required libraries
 from kafka.producer import KafkaProducer
+from kafka.consumer import KafkaConsumer
 import numpy as np
 import json
-
-raw_data_producer = KafkaProducer(bootstrap_servers='localhost:9092')
-
-#Produce synthetic data and the occasional anomalies
-for i in range(1000):
-    value = np.sin(i * 0.1)
-    
-    # Introduce a random spike as an anomaly with 1% probability
-    if np.random.rand() < 0.01:
-        value += np.random.rand() * 5
-
-    # Create a dictionary with both value and time-step
-    data = {
-        "value": value,
-        "time_step": i
-    }
-
-    # Convert the dictionary to a JSON string and encode it
-    raw_data_producer.send('raw-data', value=json.dumps(data).encode("utf-8"))
-
-raw_data_producer.flush()
-raw_data_producer.close()
-
-
-
-#Import all required libraries
-from kafka.consumer import KafkaConsumer
-
-# Create a consumer to read raw data
-raw_data_consumer = KafkaConsumer('raw-data', bootstrap_servers='localhost:9092')
-
-# Collect data for training and prediction, along with time-steps
-data = []
-time_steps = []
-for i, msg in enumerate(raw_data_consumer):
-    value = float(msg.value.decode('utf-8'))
-    data.append([value])
-    time_steps.append(i)  # Assuming each data point corresponds to a time-step
-
-    # Stop collecting after 1000 data points
-    if len(data) >= 1000:
-        break
-
-#Import all required libraries
 from sklearn.ensemble import IsolationForest
 
+def create_kafka_producer(bootstrap_servers):
+    """
+    Create and return a Kafka producer.
+    """
+    return KafkaProducer(bootstrap_servers=bootstrap_servers)
 
-# Convert to a NumPy array for Scikit-learn
-X = np.array(data)
+def produce_synthetic_data(producer, topic_name, num_data_points=1000):
+    """
+    Produce synthetic data with occasional anomalies and send it to a Kafka topic.
+    """
+    for i in range(num_data_points):
+        value = np.sin(i * 0.1)
+        
+        if np.random.rand() < 0.01:
+            value += np.random.rand() * 5
 
-# Train an Isolation Forest model
-model = IsolationForest(contamination=0.01)
-model.fit(X)
+        data = {
+            "value": value,
+            "time_step": i
+        }
+        producer.send(topic_name, value=json.dumps(data).encode("utf-8"))
 
-# Predict anomalies
-pred = model.predict(X)
+    producer.flush()
 
-# Anomalies are represented by -1
-anomalies = X[pred == -1]
-anomaly_time_steps = [time_steps[i] for i in range(len(pred)) if pred[i] == -1]
+def create_kafka_consumer(topic_name, bootstrap_servers):
+    """
+    Create and return a Kafka consumer.
+    """
+    return KafkaConsumer(topic_name, bootstrap_servers=bootstrap_servers)
+
+def collect_raw_data(consumer, max_data_points=1000):
+    """
+    Collect raw data from a Kafka topic.
+    """
+    data, time_steps = [], []
+    
+    for i, msg in enumerate(consumer):
+        value = json.loads(msg.value.decode('utf-8'))['value']
+        data.append([value])
+        time_steps.append(i)
+
+        if len(data) >= max_data_points:
+            break
+            
+    return np.array(data), time_steps
+
+def detect_anomalies(X, contamination=0.01):
+    """
+    Detect anomalies in the data using the Isolation Forest algorithm.
+    """
+    model = IsolationForest(contamination=contamination)
+    model.fit(X)
+    pred = model.predict(X)
+    anomalies = X[pred == -1]
+    anomaly_indices = [i for i, p in enumerate(pred) if p == -1]
+    return anomalies, anomaly_indices
+
+def produce_anomalies(producer, topic_name, anomalies, anomaly_time_steps):
+    """
+    Send detected anomalies to a Kafka topic.
+    """
+    for anomaly, time_step in zip(anomalies, anomaly_time_steps):
+        anomaly_data = {
+            "anomalous_value": anomaly[0],
+            "time_step": time_step
+        }
+        producer.send(topic_name, value=json.dumps(anomaly_data))
+
+    producer.flush()
 
 
-# Create a producer to send anomalies
-anomalies_producer = KafkaProducer(bootstrap_servers='localhost:9092')
+if __name__ == "__main__":
+    bootstrap_servers = 'localhost:9092'
 
-# Produce the detected anomalies and time-steps to the Redpanda topic
-for anomaly, time_step in zip(anomalies, anomaly_time_steps):
-    anomaly_data = {
-        "anomalous_value": anomaly[0],
-        "time_step": time_step
-    }
-    anomalies_producer.send('anomalies', value=json.dumps(anomaly_data))
+    # Produce synthetic data
+    raw_data_producer = create_kafka_producer(bootstrap_servers)
+    produce_synthetic_data(raw_data_producer, 'raw-data')
+    raw_data_producer.close()
 
-anomalies_producer.flush()
+    # Consume raw data
+    raw_data_consumer = create_kafka_consumer('raw-data', bootstrap_servers)
+    X, time_steps = collect_raw_data(raw_data_consumer)
 
-# Print the results along with corresponding time-steps
-print("Detected anomalies:")
-for anomaly, time_step in zip(anomalies, anomaly_time_steps):
-    print(f"Value: {anomaly[0]}, Time-step: {time_step}")
+    # Detect anomalies
+    anomalies, anomaly_time_steps = detect_anomalies(X)
 
+    # Produce anomalies
+    anomalies_producer = create_kafka_producer(bootstrap_servers)
+    produce_anomalies(anomalies_producer, 'anomalies', anomalies, anomaly_time_steps)
+    anomalies_producer.close()
 
+    # Print results
+    print("Detected anomalies:")
+    for anomaly, time_step in zip(anomalies, anomaly_time_steps):
+        print(f"Value: {anomaly[0]}, Time-step: {time_step}")
